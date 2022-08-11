2bc011078a9ba7de941fd0935ba33252ad23871e
SongE12 <aassdd6434@naver.com>
2022-08-11 22:16:56 +0900

    useEffect 사용하여 alert 만들기

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,18 +1,36 @@
-import React, { useEffect } from "react";
+import { isValidInputTimeValue } from "@testing-library/user-event/dist/utils";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function Detail(props) {
   useEffect(() => {
-    console.log("안녕");
-  });
+    setTimeout(() => {
+      setAlert(false);
+    }, 2000);
+  }, []);
 
   let { id } = useParams();
   let dataId = props.shoes.find((a) => {
     return a.id === Number(id);
   });
+  let [alert, setAlert] = useState(true);
+  let [numberAlert, setNumberAlert] = useState();
+  let [inputText, setInputText] = useState();
+
+  useEffect(() => {
+    console.log(isNaN(inputText));
+    if (isNaN(inputText)) {
+      setNumberAlert(false);
+    } else {
+      setNumberAlert(true);
+    }
+  }, [inputText]);
 
   return (
     <div className="container">
+      {alert ? (
+        <div className="alert alert-warning">2초이내 구매시 할인</div>
+      ) : null}
       <div className="row">
         <div className="col-md-6">
           <img
@@ -23,6 +41,15 @@ function Detail(props) {
             alt="shoes"
           />
         </div>
+        {!numberAlert ? (
+          <div className="alert alert-danger">경고: 숫자만입력하세요</div>
+        ) : null}
+        <input
+          type="text"
+          onChange={(e) => {
+            setInputText(e.target.value);
+          }}
+        />
         <div className="col-md-6">
           <h4 className="pt-5">{dataId.title}</h4>
           <p>{dataId.content}</p>
